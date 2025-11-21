ff9c608b3d65fc3d381efe6b29dc86888d80f7ca
syaikhulwahab <syaikhulwahab@gmail.com>
2025-11-21 09:25:01 +0700

    Add files via upload

---
+++ sidakep/Backend/config.py
@@ -0,0 +1,5 @@
+# config.py
+SPREADSHEET_ID = "<GANTI_DENGAN_SPREADSHEET_ID_ANDA>"
+DRIVE_UPLOAD_FOLDER_ID = "<GANTI_DENGAN_FOLDER_DRIVE_ID>"
+CREDENTIALS_FILE = "credentials.json" # letakkan file credentials service account Anda di folder backend
+SECRET_KEY = "change_this_to_a_random_secret"
